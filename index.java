a778a357792af75eea9da154e01dac94019d1fcb
basara24 <mayconpedroso2005@gmail.com>
2024-08-16 20:45:38 -0300

    add "CAIXINHAAA"

--- index.java
+++ index.java
@@ -2,5 +2,16 @@ public class index {
     public static void main(String[] args) {
         System.out.println("helloWord");
         System.out.println("nao");
+
+        final int caixinha = 12;
+        int tang = 23;
+        int delValle = 24;
+        int mix = tang + delValle;
+        int um = 1 , dois = 2 , tres = um + dois;
+
+        System.out.println("essa é variavel: " + caixinha);
+        System.out.println("essa é constante: " + delValle);
+        System.out.println("esse é o mix do povo " + mix);
+        System.out.println("esse é o tres: " + tres);
     }
 }
